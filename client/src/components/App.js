4828fded8a81cb9b7cc16a7f1274385fb8288596
Jaden Balogh <jadenbalogh@gmail.com>
2020-11-23 13:30:27 -0800

    Add react routing with two sample pages

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import Main from './Main';
+import NavBar from './NavBar';
+import '../stylesheets/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
