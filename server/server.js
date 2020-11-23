0e9fccee622d0a308f8712bd06fb40feb40c7b03
Jean-Philippe Abadir <jpabadir@gmail.com>
2020-11-24 00:12:18 +0100

    Port ListOrder page

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import bodyParser from 'body-parser';
+import mysql from 'mysql';
 
 const app = express();
 app.use(cors());
@@ -10,6 +11,24 @@ app.listen(3001, () => {
   console.log('Server started on port 3001');
 });
 
-app.get('/', (req, res) => {
-  res.send('Received! You sent: ');
+app.get('/listorder', (req, res) => {
+  var connection = mysql.createConnection({
+    host: 'cosc304.ok.ubc.ca',
+    user: 'jabadir',
+    password: '35589738',
+    database: 'db_jabadir',
+  });
+
+  connection.connect();
+
+  connection.query(
+    'SELECT orderId,orderDate, ordersummary.customerId, customer.firstName,customer.lastName, totalAmount from ordersummary left join customer on ordersummary.customerId = customer.customerId order by orderId',
+    (error, results, fields) => {
+      if (error) throw error;
+      console.log('The solution is: ', results);
+      res.send(results);
+    }
+  );
+
+  connection.end();
 });
