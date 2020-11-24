0ab417abfb25c23821ab681c548b64b69e8d5686
Jean-Philippe Abadir <jpabadir@gmail.com>
2020-11-24 01:02:42 +0100

    Start porting loaddata

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -2,6 +2,7 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import ListOrder from './ListOrder';
+import LoadData from './LoadData';
 
 function Main() {
   return (
@@ -9,6 +10,7 @@ function Main() {
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
       <Route exact path="/listorder" component={ListOrder} />
+      <Route exact path="/loaddata" component={LoadData} />
     </Switch>
   );
 }
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,10 +3,19 @@ import { NavLink } from 'react-router-dom';
 function NavBar() {
   return (
     <nav>
-      <ul>
-        <li><NavLink to='/'>Home</NavLink></li>
-        <li><NavLink to='/about'>About</NavLink></li>
-        <li><NavLink to='/listorder'>Listorder</NavLink></li>
+      <ul style={{ listStyleType: 'none' }}>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/listorder">Listorder</NavLink>
+        </li>
+        <li>
+          <NavLink to="/loaddata">Loaddata</NavLink>
+        </li>
       </ul>
     </nav>
   );
