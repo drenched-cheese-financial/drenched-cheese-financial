20bb512ea50cf092975d9725b90ae0830149b945
ShawnMeister <shawn.mountenay@gmail.com>
2020-11-26 11:35:01 -0800

    OrderList page fully working and styled

--- client/src/views/list-order/ListOrder.js
+++ client/src/views/list-order/ListOrder.js
@@ -1,20 +1,104 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './listOrder.scss';
 
 function ListOrder() {
 	// Declare a new state variable, which we'll call "count"
-	const [orderList, setOrderList] = useState(null);
+	const [orderList, setOrderList] = useState();
 
 	useEffect(() => {
 		// Fetch the data on load
 		axios.get('http://localhost:3001/listorder').then((response) => {
-			setOrderList(response);
+			console.log(response.data[0][1][1].price);
+			setOrderList(response.data);
 		});
 	}, []);
 
 	return (
 		<div>
-			<p>{JSON.stringify(orderList)}</p>
+			<h1>Orders</h1>
+
+			{typeof orderList !== 'undefined'
+				? orderList.map((value, index) => {
+						return (
+							<div className="tablesDiv">
+								<table>
+									<tr>
+										<th>Order ID</th>
+										<th>Order Date</th>
+										<th>Customer ID</th>
+										<th>Customer Name</th>
+										<th>Total Amount</th>
+									</tr>
+									<tr className={index % 2 === 0 ? 'rowPrimary' : ''}>
+										<td key={index}>{value[0][0].orderId}</td>
+										<td key={index}>{value[0][0].orderDate}</td>
+										<td key={index}>{value[0][0].customerId}</td>
+										<td key={index}>{value[0][0].customerName}</td>
+										<td key={index}>{value[0][0].totalAmount}</td>
+									</tr>
+								</table>
+								<table className="productTable">
+									<tr>
+										<th>Product ID</th>
+										<th>Quantity</th>
+										<th>Price</th>
+									</tr>
+									{value[1].map((product, index) => {
+										return (
+											<tr>
+												<td key={index}>{product.productId}</td>
+												<td key={index}>{product.quantity}</td>
+												<td key={index}>{product.price}</td>
+											</tr>
+										);
+									})}
+								</table>
+							</div>
+						);
+				  })
+				: ''}
+
+			{/* <div>
+				<table>
+					{orderList.map((value, index) => {
+						return (
+							<div>
+								<table>
+									<tr>
+										<th>Order ID</th>
+										<th>Order Date</th>
+										<th>Customer ID</th>
+										<th>Customer Name</th>
+										<th>Total Amount</th>
+									</tr>
+									<tr className={index % 2 === 0 ? 'rowPrimary' : ''}>
+										<td key={index}>{value.orderId}</td>
+										<td key={index}>{value.orderDate}</td>
+										<td key={index}>{value.customerId}</td>
+										<td key={index}>
+											{value.firstName + ' ' + value.lastName}
+										</td>
+										<td key={index}>{'$' + value.totalAmount.toFixed(2)}</td>
+									</tr>
+								</table>
+								<table>
+									<tr>
+										<th>Product ID</th>
+										<th>Quantity</th>
+										<th>Price</th>
+									</tr>
+									<tr>
+										<td key={index}>{value.productId}</td>
+										<td></td>
+										<td></td>
+									</tr>
+								</table>
+							</div>
+						);
+					})}
+				</table>
+			</div> */}
 		</div>
 	);
 }
--- server/server.js
+++ server/server.js
@@ -24,8 +24,9 @@ app.listen(3001, () => {
 });
 
 app.get('/listorder', (req, res) => {
-	/** Create connection, and validate that it connected successfully **/
-	const getData = async () => {
+	var allData = [];
+
+	const getOrderData = async function () {
 		try {
 			//get db connection
 			let pool = await sql.connect(dbConfig);
@@ -33,12 +34,39 @@ app.get('/listorder', (req, res) => {
 			let orderResults = await pool
 				.request()
 				.query(
-					'SELECT orderId, orderDate, ordersummary.customerId, customer.firstName,customer.lastName, totalAmount ' +
+					'  SELECT orderId,orderDate, ordersummary.customerId, customer.firstName,customer.lastName, totalAmount ' +
 						'from ordersummary left join customer on ordersummary.customerId = customer.customerId order by orderId'
 				);
 
-			res.write(JSON.stringify(orderResults));
-			res.end();
+			for (let i = 0; i < orderResults.recordset.length; i++) {
+				var orderData = [];
+				let record = orderResults.recordset[i];
+				orderData.push({
+					orderId: record.orderId,
+					orderDate: record.orderDate,
+					customerId: record.customerId,
+					customerName: record.firstName + ' ' + record.lastName,
+					totalAmount: '$' + record.totalAmount.toFixed(2),
+				});
+
+				let productResults = await pool
+					.request()
+					.input('safeOrderId', sql.VarChar, record.orderId)
+					.query(
+						`select productId, quantity, price from orderproduct where orderId=@safeOrderId order by productId`
+					);
+
+				var productData = [];
+				productResults.recordset.forEach((productRecord) => {
+					productData.push({
+						productId: productRecord.productId,
+						quantity: productRecord.quantity,
+						price: '$' + productRecord.price.toFixed(2),
+					});
+				});
+
+				allData.push([orderData, productData]);
+			}
 		} catch {
 			(error) => {
 				console.log(error);
@@ -50,7 +78,9 @@ app.get('/listorder', (req, res) => {
 		}
 	};
 
-	getData();
+	getOrderData().then(function () {
+		res.send(allData);
+	});
 });
 
 app.get('/listprod', (req, res) => {
