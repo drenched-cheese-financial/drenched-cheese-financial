3190b1eaefbbc4a52a12dba5793188eb48506aa6
Jean-Philippe Abadir <jpabadir@gmail.com>
2020-11-26 01:57:33 +0100

    😢

--- client/src/views/CustomerInfo/CustomerInfo.js
+++ client/src/views/CustomerInfo/CustomerInfo.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+function getNaturalCaseFromCamel(camelCaseString) {
+  return camelCaseString;
+}
+
 function CustomerInfo() {
   // TODO: get this from auth
   // TODO: add routing to login if this is null
@@ -18,17 +22,56 @@ function CustomerInfo() {
   }, []);
 
   if (customerInfo) {
-    const listItems = Object.entries(customerInfo).map((row) => (
-      <tr key={row[0]}>
-        <th>{row[0]}</th>
-        <td>{row[1]}</td>
-      </tr>
-    ));
     return (
       <div>
         <h3>Customer Profile</h3>
         <table border="1">
-          <tbody>{listItems}</tbody>
+          <tbody>
+            <tr>
+              <th>Id</th>
+              <td>{customerInfo.customerId}</td>
+            </tr>
+            <tr>
+              <th>First Name</th>
+              <td>{customerInfo.firstName}</td>
+            </tr>
+            <tr>
+              <th>Last Name</th>
+              <td>{customerInfo.lastName}</td>
+            </tr>
+            <tr>
+              <th>Email</th>
+              <td>{customerInfo.email}</td>
+            </tr>
+            <tr>
+              <th>Phone</th>
+              <td>{customerInfo.phonenum}</td>
+            </tr>
+            <tr>
+              <th>Address</th>
+              <td>{customerInfo.address}</td>
+            </tr>
+            <tr>
+              <th>City</th>
+              <td>{customerInfo.city}</td>
+            </tr>
+            <tr>
+              <th>State</th>
+              <td>{customerInfo.state}</td>
+            </tr>
+            <tr>
+              <th>Postal Code</th>
+              <td>{customerInfo.postalCode}</td>
+            </tr>
+            <tr>
+              <th>Country</th>
+              <td>{customerInfo.country}</td>
+            </tr>
+            <tr>
+              <th>User id</th>
+              <td>{customerInfo.userid}</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     );
