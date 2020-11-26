cd491001be5e1e39275c8dbbdba49b97441d258d
ShawnMeister <shawn.mountenay@gmail.com>
2020-11-26 13:00:51 -0800

    Fix Key prop bug on listorder and listproduct

--- client/src/views/list-order/ListOrder.js
+++ client/src/views/list-order/ListOrder.js
@@ -9,7 +9,6 @@ function ListOrder() {
 	useEffect(() => {
 		// Fetch the data on load
 		axios.get('http://localhost:3001/listorder').then((response) => {
-			console.log(response.data[0][1][1].price);
 			setOrderList(response.data);
 		});
 	}, []);
@@ -21,38 +20,49 @@ function ListOrder() {
 			{typeof orderList !== 'undefined'
 				? orderList.map((value, index) => {
 						return (
-							<div className="tablesDiv">
+							<div key={index} className="tablesDiv">
 								<table>
-									<tr>
-										<th>Order ID</th>
-										<th>Order Date</th>
-										<th>Customer ID</th>
-										<th>Customer Name</th>
-										<th>Total Amount</th>
-									</tr>
-									<tr>
-										<td key={index}>{value[0][0].orderId}</td>
-										<td key={index}>{value[0][0].orderDate}</td>
-										<td key={index}>{value[0][0].customerId}</td>
-										<td key={index}>{value[0][0].customerName}</td>
-										<td key={index}>{value[0][0].totalAmount}</td>
-									</tr>
+									<thead>
+										<tr>
+											<th>Order ID</th>
+											<th>Order Date</th>
+											<th>Customer ID</th>
+											<th>Customer Name</th>
+											<th>Total Amount</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td>{value[0][0].orderId}</td>
+											<td>{value[0][0].orderDate}</td>
+											<td>{value[0][0].customerId}</td>
+											<td>{value[0][0].customerName}</td>
+											<td>{value[0][0].totalAmount}</td>
+										</tr>
+									</tbody>
 								</table>
 								<table className="productTable">
-									<tr>
-										<th>Product ID</th>
-										<th>Quantity</th>
-										<th>Price</th>
-									</tr>
-									{value[1].map((product, index) => {
-										return (
-											<tr className={index % 2 !== 0 ? 'rowPrimary' : ''}>
-												<td key={index}>{product.productId}</td>
-												<td key={index}>{product.quantity}</td>
-												<td key={index}>{product.price}</td>
-											</tr>
-										);
-									})}
+									<thead>
+										<tr>
+											<th>Product ID</th>
+											<th>Quantity</th>
+											<th>Price</th>
+										</tr>
+									</thead>
+									<tbody>
+										{value[1].map((product, productIndex) => {
+											return (
+												<tr
+													key={productIndex}
+													className={index % 2 !== 0 ? 'rowPrimary' : ''}
+												>
+													<td>{product.productId}</td>
+													<td>{product.quantity}</td>
+													<td>{product.price}</td>
+												</tr>
+											);
+										})}
+									</tbody>
 								</table>
 							</div>
 						);
