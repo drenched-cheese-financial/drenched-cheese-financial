455f1bfde3cb98275442e0dc7ac38517b745a8e4
Jean-Philippe Abadir <jpabadir@gmail.com>
2020-11-28 00:03:21 +0100

    add check orderid validity

--- client/src/views/shipment/Shipment.js
+++ client/src/views/shipment/Shipment.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function Shipment() {
-  return <div>shipment page</div>;
+  const [orderIsValid, setOrderIsValid] = useState(null);
+  const [orderId, setOrderId] = useState(null);
+
+  const handleChange = (event) => {
+    setOrderId(event.target.value);
+  };
+
+  const addToShipment = () => {
+    axios
+      .get('http://localhost:3001/ship?orderId=' + orderId, {
+        withCredentials: true,
+      })
+      .then((response) => {
+        setOrderIsValid(response.data);
+      });
+  };
+
+  return (
+    <div>
+      {JSON.stringify(orderIsValid)}
+      <input onChange={handleChange} />
+      <button onClick={addToShipment}>add to shipment</button>
+    </div>
+  );
 }
 
 export default Shipment;
--- server/routes/ship.js
+++ server/routes/ship.js
@@ -5,9 +5,30 @@ import dbConfig from '../dbconfig.js';
 const router = express.Router();
 
 router.get('/', function (req, res) {
-  console.log(req);
-  res.end('order id passed to server is ' + req.query.orderId);
-  res.end();
+  getOrderSummaryWithOrderId(req.query.orderId)
+    .then((response) => {
+      res.send(response.recordset.length > 0);
+    })
+    .catch(() => {
+      res.end();
+    });
 });
+
+function getOrderSummaryWithOrderId(orderId) {
+  return new Promise((resolve, reject) => {
+    sql.connect(dbConfig).then((conn) => {
+      conn
+        .request()
+        .input('orderId', sql.Int, orderId)
+        .query('SELECT orderId FROM orderSummary WHERE orderId = @orderId')
+        .then((result) => {
+          resolve(result);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  });
+}
 
 export default router;
