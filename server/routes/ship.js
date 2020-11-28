62b3d560ce052caa32c54572eba752187fdd8101
Jean-Philippe Abadir <jpabadir@gmail.com>
2020-11-28 01:45:14 +0100

    connect be and fe

--- server/routes/ship.js
+++ server/routes/ship.js
@@ -7,14 +7,21 @@ const router = express.Router();
 router.get('/', async function (req, res) {
   const orderContent = await getOrderContent(req.query.orderId);
   if (orderContent.length === 0) {
-    res.send('order empty');
+    res.send({ type: 'invalid_order' });
     res.end();
   } else {
     const missingProduct = await missingInventoryProduct(orderContent);
-    console.log(missingProduct);
+    if (missingProduct) {
+      res.send({ type: 'missing_inventory', data: missingProduct });
+      res.end();
+    } else {
+      res.send({ type: 'succcess', data: 'all good!' });
+      res.end();
+    }
   }
 });
 
+// Return the ids and quantities of the products in the specified order
 function getOrderContent(orderId) {
   return new Promise((resolve, reject) => {
     sql.connect(dbConfig).then((conn) => {
@@ -34,6 +41,9 @@ function getOrderContent(orderId) {
   });
 }
 
+// Returns the ID of the first product it finds whose warehouse 1 inventory
+// is less than the needed quantity, or null if the inventory is sufficient for
+// all products
 function missingInventoryProduct(orderedProducts) {
   var processedProducts = 0;
   return new Promise((resolve, reject) => {
@@ -49,13 +59,13 @@ function missingInventoryProduct(orderedProducts) {
             if (result.recordset[0].quantity < product.quantity) {
               resolve(product.productId);
             }
+            if (++processedProducts == orderedProducts.length) {
+              resolve(null);
+            }
           })
           .catch((err) => {
             reject(err);
           });
-        if (++processedProducts == orderedProducts.length) {
-          resolve(null);
-        }
       });
     });
   });
