905c38f5eadaedd747779cc090da7ba8754b183f
ShawnMeister <shawn.mountenay@gmail.com>
2020-11-29 12:46:15 -0800

    Change wording to look more errory

--- client/src/views/login/Login.js
+++ client/src/views/login/Login.js
@@ -4,14 +4,15 @@ import ValidationForm from '../../components/validation/ValidationForm';
 
 function Login() {
   const history = useHistory();
-  
+
   const handleValidate = () => {
     history.push('/');
-  }
+  };
 
   return (
     <div>
-      <h1>Enter your credentials to login:</h1>
+      <h1>Customer Login</h1>
+      <h6>Credentials Required for Access</h6>
       <ValidationForm onValidate={handleValidate} />
     </div>
   );
