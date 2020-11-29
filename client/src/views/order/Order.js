35d0bab9f4e3ea14bc2dfac84a3eef51db52c5c2
ShawnMeister <shawn.mountenay@gmail.com>
2020-11-29 00:05:04 -0800

    Changed cosmetics by adjusting html tag plcements (your order summary)

--- client/src/views/order/Order.js
+++ client/src/views/order/Order.js
@@ -4,47 +4,58 @@ import axios from 'axios';
 import OrderTable from './OrderTable';
 
 function Order() {
-  let { customerId } = useParams();
-  const [order, setOrder] = useState();
-  const [orderJSX, setOrderJSX] = useState();
+	let { customerId } = useParams();
+	const [order, setOrder] = useState();
+	const [orderJSX, setOrderJSX] = useState();
 
-  const completeOrder = () => {
-    let params = new URLSearchParams('customerId=' + customerId);
-    axios.get('http://localhost:3001/order?' + params, { withCredentials: true }).then((res) => {
-      setOrder(res.data);
-    });
-  };
+	const completeOrder = () => {
+		let params = new URLSearchParams('customerId=' + customerId);
+		axios
+			.get('http://localhost:3001/order?' + params, { withCredentials: true })
+			.then((res) => {
+				setOrder(res.data);
+			});
+	};
 
-  const renderOrder = () => {
-    if (order) {
-      if (order.err) {
-        setOrderJSX(<div>{order.err}</div>);
-      } else {
-        setOrderJSX(
-          <div>
-            <OrderTable products={order.products}></OrderTable>
-            <p>Order completed. Product will be shipped shortly.</p>
-            <p>Order reference number: {order.id}</p>
-            <p>
-              Shipped to {order.customer.firstName} {order.customer.lastName} (id {order.customer.id})
-            </p>
-          </div>
-        );
-      }
-    } else {
-      setOrderJSX(<div>Failed to load order!</div>);
-    }
-  };
+	const renderOrder = () => {
+		if (order) {
+			if (order.err) {
+				setOrderJSX(
+					<div>
+						<br />
+						<br />
+						{order.err}
+					</div>
+				);
+			} else {
+				setOrderJSX(
+					<div>
+						<h1>Your Order Summary</h1>
+						<OrderTable products={order.products}></OrderTable>
+						<p>Order completed. Product will be shipped shortly.</p>
+						<p>Order reference number: {order.id}</p>
+						<p>
+							Shipped to {order.customer.firstName} {order.customer.lastName}{' '}
+							(id {order.customer.id})
+						</p>
+					</div>
+				);
+			}
+		} else {
+			setOrderJSX(
+				<div>
+					<br />
+					<br />
+					Failed to load order!
+				</div>
+			);
+		}
+	};
 
-  useEffect(completeOrder, [customerId]);
-  useEffect(renderOrder, [order]);
+	useEffect(completeOrder, [customerId]);
+	useEffect(renderOrder, [order]);
 
-  return (
-    <div>
-      <h1>Your Order Summary</h1>
-      {orderJSX}
-    </div>
-  );
+	return <div>{orderJSX}</div>;
 }
 
 export default Order;
