863ff519c5ff6785a66505df2e9ade54e6bc6c1e
Jaden Balogh <jadenbalogh@gmail.com>
2020-11-28 22:34:29 -0800

    Fix incorrect folder and file capitalization on origin

---
+++ client/src/app/App.js
@@ -0,0 +1,14 @@
+import Main from '../components/main/Main';
+import NavBar from '../components/navbar/Navbar';
+import './app.scss';
+
+function App() {
+  return (
+    <div className='App'>
+      <NavBar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
