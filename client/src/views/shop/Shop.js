5d03824205217a50ef5d0c25b75bc12779820b62
Jaden Balogh <jadenbalogh@gmail.com>
2020-12-05 20:49:25 -0800

    Fix merge bugs and update visuals

--- client/src/views/shop/Shop.js
+++ client/src/views/shop/Shop.js
@@ -1,50 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useHistory } from 'react-router-dom';
-import axios from 'axios';
-import DaBoys from '../../assets/images/daBoys/daBoysFlat.png';
 import './shop.scss';
 
 function Shop() {
   const history = useHistory();
-  const [username, setUsername] = useState();
-  const [usernameJSX, setUsernameJSX] = useState();
 
-  const fetchUsername = () => {
-    axios
-      .get('http://localhost:3001/customer', {
-        withCredentials: true,
-      })
-      .then((res) => {
-        setUsername(res.data.username);
-      });
+  const handleShop = () => {
+    history.push('/listprod');
   };
-
-  const renderUsername = () => {
-    if (username) {
-      setUsernameJSX(<p>You are signed in as {username}</p>);
-    } else {
-      setUsernameJSX();
-    }
-  };
-
-  useEffect(fetchUsername, []);
-  useEffect(renderUsername, [username]);
 
   return (
     <div className='shop'>
-      <br />
-      <img id='daBoys' src={DaBoys} />
-      <br />
-
-      <button
-        onClick={() => {
-          history.push('/listprod');
-        }}
-      >
-        Get Drenched
-      </button>
-
-      {/* {usernameJSX} */}
+      <div className='bois'>
+        <button onClick={handleShop}>Get Drenched</button>
+      </div>
     </div>
   );
 }
